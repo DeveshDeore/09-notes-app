9d752f0a1d17a94aa28afd901332fdfd6e88140f
DeveshDeore <deveshdeore82@gmail.com>
2026-03-28 16:42:46 +0530

    Add Delete Button to delete task

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,26 @@ function App() {
 
   const [task, setTask] = useState([])
 
+  //add task
   const submitHandler = (e) => {
     e.preventDefault();
     
-
     const copyTask = [...task];
-
     copyTask.push({ title, details})
-
     setTask(copyTask)
 
     setTitle('')
     setDetails('')
   };
+  
+//  delete taks
+  const deleteNote=(idx)=>{
+    const copyTask =[...task];
+    
+    copyTask.splice(idx,1)
+    setTask(copyTask)
+
+  }
 
   return (
     <>
@@ -29,7 +36,7 @@ function App() {
         <form
           onSubmit={(e) => {
             submitHandler(e);
-            let hed={title}
+            
           }}
           className="flex gap-4 lg:w-1/2 p-10 flex-col items-start"
         >
@@ -41,7 +48,7 @@ function App() {
             value={title}
             onChange={(e)=>{
               setTitle(e.target.value)
-              console.log(title)
+            
              }}
             className="px-5 w-full font-medium outline-none  py-2 border-2 rounded"
           />
@@ -64,11 +71,17 @@ function App() {
         </form>
         <div className="  lg:w-1/2 lg:border-l-2  p-10 h-full">
           <h1 className="text-3xl font-bold text-center">Recent Notes</h1>
-          <div className="flex flex-wrap items-start gap-5 mt-5 h-full overflow-auto ">
+          <div className="flex flex-wrap items-start gap-5 mt-5 h-[90%] overflow-auto ">
             {task.map(function(elem, idx){
-              return <div key={idx} className="h-52 w-40 text-black rounded-xl p-4 bg-white">
-                <h3 className="leading-tight text-xl font-bold">{elem.title}</h3>
+              return <div key={idx} className="flex justify-between flex-col items-start relative h-52 w-40 text-black bg-cover rounded-xl pt-9 pb-4 px-4 bg-[url('https://static.vecteezy.com/system/resources/thumbnails/010/793/873/small/a-lined-note-paper-covered-with-transparent-tape-on-a-yellow-background-with-a-white-checkered-pattern-free-png.png')]">
+                <div>
+                  <h3 className="leading-tight text-xl font-bold">{elem.title}</h3>
                 <p className="mt-2 leading-tight font-medium text-gray-700">{elem.details}</p>
+              
+                </div>
+                 <button onClick={()=>{
+                  deleteNote(idx)
+                 }} className="w-full bg-red-600 py-1 text-xs rounded font-bold cursor-pointer active:scale-95 text-white">Delete</button>
               </div>
             })}
            
